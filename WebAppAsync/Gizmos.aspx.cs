d2cf4776beecf64c3aedf02781f050a2d29cb6f3
ameurm <ameur.mezaache@gmail.com>
2016-11-25 16:18:27 -0500

    Add project files.

---
+++ WebAppAsync/Gizmos.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebAppAsync.Service;
+
+namespace WebAppAsync
+{
+    public partial class Gismos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var gizmoService = new GizmoService();
+            GizmoGridView.DataSource = gizmoService.GetGizmos();
+           // GizmoGridView.DataSource = gizmoService.GetGizmosAsync();
+            GizmoGridView.DataBind();
+        }
+    }
+}
